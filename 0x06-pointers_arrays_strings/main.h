f1b6219bd2fabfd2f1be7345c3072377d45c9a6c
Desolution01 <jas3610@gmail.com>
2022-12-21 09:25:02 -0800

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-nt _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +11,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+
+#endif 
