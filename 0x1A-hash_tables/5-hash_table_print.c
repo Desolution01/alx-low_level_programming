ee69a9f59387e507d06b77b7ddd2cd929abb07d6
Desolution01 <jas3610@gmail.com>
2023-04-08 15:45:37 -0700

    0x1A-hash_tables updated

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,46 @@
+#include "./hash_tables.h"
+
+/**
+ * Author: Aanuoluwapo Shodipo
+ * Date Created: 08/04/2023
+ * Last Modified: 08/04/2023
+ *
+ * Description
+ * A function that retrieves a value associated with a key.
+ *
+ * Usage
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ * Description: Key/value pairs are printed in the order
+ * they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#iinclude "hash_tables.h"
+
+/**
+ * Author: Aanuoluwapo Shodipo
+ * Date Created: 08/04/2023
+ * Last Modified: 08/04/2023
+ *
+ * Description 
+ * A function that deletes a hash table.
+ *
+ * Usage
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
